9933336fd59c75706398e3694aa3abb1bd6fff5b
Simon Roth <sroth@sroth-puzzle.local>
2019-03-29 14:46:34 +0100

    final

--- src/main/java/com/example/demo/user/control/UserController.java
+++ src/main/java/com/example/demo/user/control/UserController.java
@@ -2,14 +2,17 @@ package com.example.demo.user.control;
 
 import com.example.demo.user.Entity.User;
 import com.example.demo.user.repository.UserRepo;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.Arrays;
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
@@ -22,13 +25,19 @@ public class UserController {
 
     @GetMapping
     public ResponseEntity<Iterable<User>> getAll(){
-        Iterable<User> users = this.userRepo.findAll();
+        Iterable<User> users = userRepo.findAll();
         return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity getUserById(@PathVariable Long id) {
+        User boyyyyy = userRepo.findById(id).orElseThrow(EntityNotFoundException::new);
+        return ResponseEntity.ok(boyyyyy);
     }
 
     @PostMapping
     public ResponseEntity<User> create(User user){
-        this.userRepo.save(user);
+        userRepo.save(user);
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
